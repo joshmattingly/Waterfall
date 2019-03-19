767576a29bad5601b54e44705a9eb6424961cb99
Josh Mattingly <josh.mattingly@conagra.com>
2019-03-18 21:38:52 -0500

    Added xAxis word wrapping
    https://bl.ocks.org/guypursey/f47d8cd11a8ff24854305505dbbd8c07

--- waterfall.js
+++ waterfall.js
@@ -1,5 +1,5 @@
 
-var margin = {top: 20, right: 10, bottom: 20, left: 10};
+var margin = {top: 160, right: 10, bottom: 80, left: 73};
 
 var width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
@@ -27,7 +27,7 @@ d3.csv("sample_data.csv").then(function(dataset){
         dataset[i].class = (dataset[i].value >= 0) ? 'positive' : 'negative';
     }
     dataset.push({
-        name: 'Total',
+        Category: 'Current Year',
         end: cumulative,
         start: 0,
         class: 'total'
@@ -40,9 +40,11 @@ d3.csv("sample_data.csv").then(function(dataset){
         .range([height, 0]);
 
     let xScale = d3.scaleBand()
+        .domain(dataset.map(d => d.Category))
         .range([0, width]);
 
     let xAxis = d3.axisBottom(xScale);
+    let yAxis = d3.axisLeft(yScale);
 
     svg.selectAll('rect')
         .data(dataset)
@@ -51,7 +53,7 @@ d3.csv("sample_data.csv").then(function(dataset){
         .attr("x", function(d,i){ return i * (width / dataset.length); })
         .attr("y", function(d){return yScale(Math.max(d.start, d.end)); })
         .attr("height", function(d){ return Math.abs(yScale(d.end) - yScale(d.start)); })
-        .attr("width", 50)
+        .attr("width", (width / dataset.length)-8)
         .attr('fill', function(d){
             if(d.Category == 'Last Year' || d.Category == 'Current Year'){
                 return 'grey';
@@ -65,6 +67,35 @@ d3.csv("sample_data.csv").then(function(dataset){
 
     svg.append("g")
         .call(xAxis)
-        .attr("transform", "translate(0, " + height + ")");
+        .attr("transform", "translate(0, " + height + ")")
+        .selectAll(".tick text")
+        .call(wrap, xScale.bandwidth());
 
+    svg.append("g")
+        .call(yAxis)
+        .selectAll(".tick text");
 });
+
+function wrap(text, width) {
+    text.each(function() {
+        var text = d3.select(this),
+            words = text.text().split(/\s+/).reverse(),
+            word,
+            line = [],
+            lineNumber = 0,
+            lineHeight = 1.1, // ems
+            y = text.attr("y"),
+            dy = parseFloat(text.attr("dy")),
+            tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em")
+        while (word = words.pop()) {
+            line.push(word)
+            tspan.text(line.join(" "))
+            if (tspan.node().getComputedTextLength() > width) {
+                line.pop()
+                tspan.text(line.join(" "))
+                line = [word]
+                tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", `${++lineNumber * lineHeight + dy}em`).text(word)
+            }
+        }
+    })
+}
